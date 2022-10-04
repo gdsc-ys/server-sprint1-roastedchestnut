7834bbf310c7998933b7d8adb9a076f10fc38552
roastedchestnut <blaze@yonsei.ac.kr>
2022-10-04 15:46:45 +0900

    feat: Add custom HTTP header to measure response time

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 from routers import history, management, manager, user, vehicle
+
 
 app = FastAPI()
 
@@ -8,6 +11,15 @@ app.include_router(vehicle.router)
 app.include_router(manager.router)
 app.include_router(history.router)
 app.include_router(management.router)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 @app.get("/")
